fc75ad090f8bc31ab68b552db04be373ca9c3701
yofujtisu <yofujitsuuu@gmail.com>
2024-05-05 23:56:06 +0300

    railway test

--- lootheaven/src/App.jsx
+++ lootheaven/src/App.jsx
@@ -3,7 +3,7 @@ import { Home } from './components/home/Home'
 import { Navbar } from './components/navbar/Navbar'
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
 import { AdminTable } from './components/admin/AdminTable'
-import { CreateItem } from './components/catalog/CreateItem'
+// import { CreateItem } from './components/catalog/CreateItem'
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import UserProfile from './components/user/UserProfile'
 import { Login } from './components/auth/Login'
@@ -21,7 +21,7 @@ function App() {
           <Route path='/home' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/loots' element={<LootsList endpointSuffix={""} />} />
-          <Route path='/loots/create' element={<PrivateRoute><CreateItem /></PrivateRoute>} />
+          {/* <Route path='/loots/create' element={<PrivateRoute><CreateItem /></PrivateRoute>} /> */}
           <Route path='/me' element={<PrivateRoute><UserProfile /></PrivateRoute>} />
           <Route path='/users/:id' element={<User/>} />
           <Route path='/admin' element={<PrivateRoute><AdminTable /></PrivateRoute>} />
--- lootheaven/src/components/auth/Logout.jsx
+++ lootheaven/src/components/auth/Logout.jsx
@@ -2,6 +2,7 @@ export const Logout = () => {
     fetch('http://localhost:8082/logout', { method: 'POST', credentials: 'include' })
         .then(() => {
             window.location.href = "/home"; 
+            window.location.reload;
         })
         .catch(error => console.error('Error logging out:', error));
 };
